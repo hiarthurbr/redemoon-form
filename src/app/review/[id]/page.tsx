5588f2b32f496d15d7e0ec1c0f5c1f5cf07a321a
hiarthurbr <hi@arthurbr.me>
2025-05-16 23:08:32 -0300

    fix formatting

--- src/app/review/[id]/page.tsx
+++ src/app/review/[id]/page.tsx
@@ -26,7 +26,6 @@ import {
   Divider,
   Tooltip,
 } from "@heroui/react";
-
 import { z, type ZodSchema } from "zod";
 import { create } from "zustand";
 
@@ -135,12 +134,12 @@ export default function CharacterFormEditor() {
 
   return (
     <div className="space-y-6 max-w-4xl mx-auto prose">
-      <Tooltip content="I am a tooltip" showArrow placement="right">
+      <Tooltip showArrow content="I am a tooltip" placement="right">
         <Snippet
           classNames={{ pre: "inline-flex items-center gap-4" }}
           disableCopy={id == null}
-          onCopy={() => setId(humanId())}
           symbol="#"
+          onCopy={() => setId(humanId())}
         >
           {id ?? (
             <CircularProgress
@@ -160,15 +159,15 @@ export default function CharacterFormEditor() {
           <div className="flex items-center gap-4">
             <div className="grid grid-rows-1 place-items-center max-md:grid-rows-3 gap-4">
               <Image
-                as={Avatar}
-                radius="full"
-                showFallback
                 isBlurred
+                showFallback
                 alt="Discord Profile"
+                as={Avatar}
                 className="size-20 ml-4"
                 classNames={{
                   wrapper: "row-span-2",
                 }}
+                radius="full"
                 src={formData.discordProfileImage}
               />
               <Button
@@ -186,21 +185,13 @@ export default function CharacterFormEditor() {
                   <Input
                     disabled
                     readOnly
-                    classNames={{
-                      base: "flex-row",
-                      inputWrapper:
-                        "bg-transparent data-[hover=true]:bg-transparent",
-                    }}
-                    label="Discord Username"
-                    value={formData.discordUsername}
-                    size="sm"
                     // @ts-expect-error
                     as={(props) => {
                       return (
                         <Snippet
+                          codeString={formData.discordUsername}
                           color="primary"
                           symbol=""
-                          codeString={formData.discordUsername}
                           {...{
                             ...props,
                             children:
@@ -212,29 +203,37 @@ export default function CharacterFormEditor() {
                         />
                       );
                     }}
+                    classNames={{
+                      base: "flex-row",
+                      inputWrapper:
+                        "bg-transparent data-[hover=true]:bg-transparent",
+                    }}
+                    label="Discord Username"
+                    size="sm"
+                    value={formData.discordUsername}
                   />
                 </div>
                 <div className="space-y-2">
                   <Input
                     disabled
                     readOnly
+                    // @ts-expect-error
+                    as={(props) => (
+                      <Snippet
+                        codeString={formData.discordId}
+                        color="primary"
+                        symbol=""
+                        {...props}
+                      />
+                    )}
                     classNames={{
                       base: "flex-row",
                       inputWrapper:
                         "bg-transparent data-[hover=true]:bg-transparent",
                     }}
                     label="Discord Id"
-                    value={formData.discordId}
                     size="sm"
-                    // @ts-expect-error
-                    as={(props) => (
-                      <Snippet
-                        symbol=""
-                        color="primary"
-                        codeString={formData.discordId}
-                        {...props}
-                      />
-                    )}
+                    value={formData.discordId}
                   />
                 </div>
               </div>
@@ -242,23 +241,23 @@ export default function CharacterFormEditor() {
                 <Input
                   disabled
                   readOnly
+                  // @ts-expect-error
+                  as={(props) => (
+                    <Snippet
+                      codeString={formData.discordName}
+                      color="primary"
+                      symbol=""
+                      {...props}
+                    />
+                  )}
                   classNames={{
                     base: "flex-row",
                     inputWrapper:
                       "bg-transparent data-[hover=true]:bg-transparent",
                   }}
                   label="Discord Name"
-                  value={formData.discordName}
                   size="sm"
-                  // @ts-expect-error
-                  as={(props) => (
-                    <Snippet
-                      symbol=""
-                      color="primary"
-                      codeString={formData.discordName}
-                      {...props}
-                    />
-                  )}
+                  value={formData.discordName}
                 />
                 <Button
                   className="mt-0! max-md:hidden"
@@ -280,17 +279,6 @@ export default function CharacterFormEditor() {
         </CardHeader>
         <CardBody className="space-y-4">
           <Accordion
-            onSelectionChange={(set) =>
-              setMinecraftAccount((obj) => {
-                obj.bedrock = false;
-                obj.java = false;
-                for (const key of Array.from(set)) {
-                  // @ts-expect-error
-                  obj[key] = true;
-                }
-                return { ...obj };
-              })
-            }
             selectedKeys={
               new Set(
                 Object.entries(minecraftAccount)
@@ -299,9 +287,22 @@ export default function CharacterFormEditor() {
               )
             }
             selectionMode="multiple"
+            onSelectionChange={(set) =>
+              setMinecraftAccount((obj) => {
+                obj.bedrock = false;
+                obj.java = false;
+                for (const key of Array.from(set)) {
+                  // @ts-expect-error
+                  obj[key] = true;
+                }
+
+                return { ...obj };
+              })
+            }
           >
             <AccordionItem
               key="java"
+              textValue="Java Edition"
               title={
                 <Checkbox
                   isSelected={minecraftAccount.java}
@@ -315,35 +316,34 @@ export default function CharacterFormEditor() {
                   Java Edition
                 </Checkbox>
               }
-              textValue="Java Edition"
             >
               <Tabs
                 className="md:min-h-36 max-md:w-full max-md:flex justify-center"
+                classNames={{ panel: "md:pl-12" }}
                 color="success"
                 isVertical={!verticalLayout}
-                classNames={{ panel: "md:pl-12" }}
               >
                 <Tab
                   key="original"
-                  title="Original Account"
                   className="data-[selected=true]:font-semibold"
+                  title="Original Account"
                 >
                   <div className="grid grid-rows-1 md:grid-rows-2 gap-4 place-items-center">
                     <Button
-                      color="success"
-                      variant="flat"
                       className="font-semibold"
+                      color="success"
                       startContent={
                         <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 50 50"
+                          className="size-10"
                           fill="currentColor"
                           role="presentation"
-                          className="size-10"
+                          viewBox="0 0 50 50"
+                          xmlns="http://www.w3.org/2000/svg"
                         >
                           <path d="M 5 4 C 4.448 4 4 4.447 4 5 L 4 24 L 24 24 L 24 4 L 5 4 z M 26 4 L 26 24 L 46 24 L 46 5 C 46 4.447 45.552 4 45 4 L 26 4 z M 4 26 L 4 45 C 4 45.553 4.448 46 5 46 L 24 46 L 24 26 L 4 26 z M 26 26 L 26 46 L 45 46 C 45.552 46 46 45.553 46 45 L 46 26 L 26 26 z" />
                         </svg>
                       }
+                      variant="flat"
                     >
                       Link Microsoft Account
                     </Button>
@@ -361,16 +361,16 @@ export default function CharacterFormEditor() {
                       }}
                       description={
                         <Skeleton
-                          isLoaded={id != null}
                           className="rounded-large my-0.5"
+                          isLoaded={id != null}
                         >
                           Premium account
                         </Skeleton>
                       }
                       name={
                         <Skeleton
-                          isLoaded={id != null}
                           className="rounded-large my-0.5"
+                          isLoaded={id != null}
                         >
                           {formData.minecraftJavaUsername}
                         </Skeleton>
@@ -380,27 +380,27 @@ export default function CharacterFormEditor() {
                 </Tab>
                 <Tab
                   key="cracked"
-                  title="Cracked Account"
                   className="data-[selected=true]:font-semibold"
+                  title="Cracked Account"
                 >
                   <div className="grid grid-rows-2 gap-4">
                     <Input
-                      label="Minecraft Java Username"
-                      name="javaUsername"
-                      color="success"
                       classNames={{
                         inputWrapper:
                           "data-[hover=true]:bg-success-100 group-data-[focus=true]:bg-success-100",
                       }}
+                      color="success"
+                      label="Minecraft Java Username"
+                      name="javaUsername"
                       value={formData.minecraftJavaUsername}
                       onValueChange={handleFormChange("minecraftUsername")}
                     />
                     <Alert
                       color="warning"
-                      variant="solid"
                       title={
                         "Make sure to double check if your username is correct"
                       }
+                      variant="solid"
                     />
                   </div>
                 </Tab>
@@ -408,6 +408,8 @@ export default function CharacterFormEditor() {
             </AccordionItem>
             <AccordionItem
               key="bedrock"
+              classNames={{ content: "flex max-md:justify-center md:pl-16" }}
+              textValue="Bedrock Edition"
               title={
                 <Checkbox
                   isSelected={minecraftAccount.bedrock}
@@ -421,25 +423,23 @@ export default function CharacterFormEditor() {
                   Bedrock Edition
                 </Checkbox>
               }
-              textValue="Bedrock Edition"
-              classNames={{ content: "flex max-md:justify-center md:pl-16" }}
             >
               <Button
-                color="success"
-                variant="flat"
                 className="font-semibold"
+                color="success"
                 startContent={
                   <svg
                     className="size-10 pointer-events-none"
-                    viewBox="0 0 32 32"
-                    version="1.1"
                     fill="currentColor"
+                    version="1.1"
+                    viewBox="0 0 32 32"
                     xmlns="http://www.w3.org/2000/svg"
                   >
                     <title>xbox</title>
                     <path d="M16 5.425c-1.888-1.125-4.106-1.922-6.473-2.249l-0.092-0.010c-0.070-0.005-0.152-0.008-0.234-0.008-0.613 0-1.188 0.16-1.687 0.441l0.017-0.009c2.357-1.634 5.277-2.61 8.426-2.61 0.008 0 0.016 0 0.024 0h0.019c0.005 0 0.011 0 0.018 0 3.157 0 6.086 0.976 8.501 2.642l-0.050-0.033c-0.478-0.272-1.051-0.433-1.662-0.433-0.085 0-0.169 0.003-0.252 0.009l0.011-0.001c-2.459 0.336-4.677 1.13-6.648 2.297l0.082-0.045zM5.554 5.268c-0.041 0.014-0.077 0.032-0.11 0.054l0.002-0.001c-2.758 2.723-4.466 6.504-4.466 10.684 0 3.584 1.256 6.875 3.353 9.457l-0.022-0.028c-1.754-3.261 4.48-12.455 7.61-16.159-3.53-3.521-5.277-4.062-6.015-4.062-0.010-0-0.021-0.001-0.032-0.001-0.115 0-0.225 0.021-0.326 0.060l0.006-0.002zM20.083 9.275c3.129 3.706 9.367 12.908 7.605 16.161 2.075-2.554 3.332-5.845 3.332-9.43 0-4.181-1.709-7.962-4.467-10.684l-0.002-0.002c-0.029-0.021-0.063-0.039-0.1-0.052l-0.003-0.001c-0.1-0.036-0.216-0.056-0.336-0.056-0.005 0-0.011 0-0.016 0h0.001c-0.741-0-2.485 0.543-6.014 4.063zM6.114 27.306c2.627 2.306 6.093 3.714 9.888 3.714s7.261-1.407 9.905-3.728l-0.017 0.015c2.349-2.393-5.402-10.901-9.89-14.29-4.483 3.39-12.24 11.897-9.886 14.29z" />
                   </svg>
                 }
+                variant="flat"
               >
                 Link Xbox Account
               </Button>
@@ -457,21 +457,21 @@ export default function CharacterFormEditor() {
             <div className="space-y-2">
               <Input
                 color="secondary"
-                variant="bordered"
                 label="Character Name"
                 name="characterName"
                 value={formData.characterName}
+                variant="bordered"
                 onValueChange={handleFormChange("characterName")}
               />
             </div>
             <div className="space-y-2">
               <NumberInput
                 color="secondary"
-                variant="bordered"
                 label="Age"
+                min={0}
                 name="age"
                 value={formData.age}
-                min={0}
+                variant="bordered"
                 onValueChange={handleFormChange("age", age_schema)}
               />
             </div>
@@ -481,9 +481,9 @@ export default function CharacterFormEditor() {
             <div className="space-y-2">
               <Select
                 color="secondary"
-                variant="bordered"
                 label="Select gender"
                 value={formData.gender}
+                variant="bordered"
                 onSelectionChange={handleFormChange("gender")}
               >
                 <SelectItem key="female">Female</SelectItem>
@@ -495,9 +495,9 @@ export default function CharacterFormEditor() {
             <div className="space-y-2">
               <Select
                 color="secondary"
-                variant="bordered"
                 label="Select pronouns"
                 value={formData.pronouns}
+                variant="bordered"
                 onSelectionChange={handleFormChange("pronouns")}
               >
                 <SelectItem key="she-her">She/Her</SelectItem>
@@ -512,9 +512,9 @@ export default function CharacterFormEditor() {
             <div className="space-y-2">
               <Select
                 color="secondary"
-                variant="bordered"
                 label="Select sexuality"
                 value={formData.sexuality}
+                variant="bordered"
                 onSelectionChange={handleFormChange("sexuality")}
               >
                 <SelectItem key="Heterosexual">Heterosexual</SelectItem>
@@ -527,9 +527,9 @@ export default function CharacterFormEditor() {
             <div className="space-y-2">
               <Select
                 color="secondary"
-                variant="bordered"
                 label="Select race"
                 value={formData.race}
+                variant="bordered"
                 onSelectionChange={handleFormChange("race")}
               >
                 <SelectItem key="angel">Angel</SelectItem>
@@ -547,11 +547,11 @@ export default function CharacterFormEditor() {
           <div className="space-y-2">
             <Input
               color="secondary"
-              variant="underlined"
               label="Origin"
               name="origin"
               placeholder="Describe your character's place of origin"
               value={formData.origin}
+              variant="underlined"
               onValueChange={handleFormChange("origin")}
             />
           </div>
@@ -577,10 +577,10 @@ export default function CharacterFormEditor() {
               </Tab>
               <Tab title="List">
                 <DynamicList
-                  items={formData.fearsList}
-                  onChange={handleFormChange("fearsList", list_item_schema)}
-                  placeholder="Enter a fear"
                   descriptionPlaceholder="Describe why the character has this fear..."
+                  items={formData.fearsList}
+                  placeholder="Enter a fear"
+                  onChange={handleFormChange("fearsList", list_item_schema)}
                 />
               </Tab>
             </Tabs>
